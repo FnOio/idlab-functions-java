4dd5b7167b201cdd26960f73f269a42b004751b9
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2022-12-02 15:10:12 +0100

    Use enum iso string for charset

--- src/main/java/be/ugent/knows/idlabFunctions/IDLabFunctions.java
+++ src/main/java/be/ugent/knows/idlabFunctions/IDLabFunctions.java
@@ -75,7 +75,7 @@ public class IDLabFunctions {
     public static List<String> dbpediaSpotlight(String text, String endpoint) {
         if (!text.equals("")) {
             try {
-                URL url = new URL(endpoint + "/annotate?text=" + URLEncoder.encode(text, "UTF-8"));
+                URL url = new URL(endpoint + "/annotate?text=" + URLEncoder.encode(text, StandardCharsets.UTF_8));
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
                 con.setRequestMethod("GET");
                 con.setRequestProperty("Accept", "application/json");
